b7faa3dd8591f2dc284fb00c2a6d62b8eb6d5f7b
alexandrujeman <alexandrujeman.ja@gmail.com>
2019-10-14 13:24:08 +0300

    Add reverse string variation non array

---
+++ reverseString_2.js
@@ -0,0 +1,11 @@
+function reverseString(str) {
+  let reversed = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+  return reversed;
+}
+
+let string = "hello";
+
+console.log(reverseString(string));
