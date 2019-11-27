b7e365faedd01131c7aa279dba307f74be7b7fc4
alexandrujeman <alexandrujeman.ja@gmail.com>
2019-11-27 11:29:49 +0200

    Add O(n) TwoSum for num and index versions

---
+++ two-sum/twoSum.js
@@ -0,0 +1,21 @@
+// O(n)
+// Replace comments for index version
+function twoNumberSum(array, targetSum) {
+  const previousValues = {};
+  let i = 0;
+  while (i < array.length) {
+    const potentialMatch = targetSum - array[i]; // const previousIndex = previousValues[potentialMatch];
+    if (previousValues[potentialMatch]) { // replace with if (previousIndex != null)
+      return [potentialMatch, array[i]]; // replace with return [previousIndex, i];
+    } else {
+      previousValues[array[i]] = true; // replace true with i
+    }
+    i++;
+  }
+  return [];
+}
+
+let arr = [6, 4];
+let num = 10;
+
+twoNumberSum(arr, num)
