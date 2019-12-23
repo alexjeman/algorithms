5b737068e30a6de4f948b25eae49b5e082503dec
alexandrujeman <alexandrujeman.ja@gmail.com>
2019-12-23 14:54:37 +0200

    Add insertion sort

---
+++ insertion-sort/insertionSort.js
@@ -0,0 +1,14 @@
+function insertionSort(array) {
+  for (let i = 1; i < array.length; i++) {
+    let j = i;
+    while (j > 0 && array[j] < array[j - 1]) {
+      let tmp = array[j];
+      array[j] = array[j - 1];
+      array[j - 1] = tmp;
+      j -= 1;
+    }
+  }
+  return array;
+}
+
+console.log(insertionSort([24, 26, 47, 2, 5, 6, 8, 9, 12, 15, 20]));
---
+++ insertion-sort/insertionSort.py
@@ -0,0 +1,10 @@
+def insertionSort(array):
+    for i in range(1, len(array)):
+        j = i
+        while j > 0 and array[j] < array[j - 1]:
+            array[j], array[j - 1] = array[j - 1], array[j]
+            j -= 1
+    return array
+
+
+print(insertionSort([24, 26, 47, 2, 5, 6, 8, 9, 12, 15, 20]))
