b005dd45fa8b2e3bd667f8311f2abf3f1acf0bf3
alexandrujeman <alexandrujeman.ja@gmail.com>
2019-12-29 14:08:27 +0200

    Add reverse string "array" . py

---
+++ reverse-string/reverseString.py
@@ -0,0 +1,13 @@
+def reverseString(arr):
+    i = 0
+    j = len(arr) - 1
+    while j > i:
+        tmp = arr[i]
+        arr[i] = arr[j]
+        arr[j] = tmp
+        i += 1
+        j -= 1
+    return arr
+
+
+print(reverseString(["h", "e", "l", "l", "o"]))
