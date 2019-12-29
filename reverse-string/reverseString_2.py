a485c02e9988140c08ae7c23655447640a5aaf40
alexandrujeman <alexandrujeman.ja@gmail.com>
2019-12-29 14:07:30 +0200

    Fix version 2 reverse string

--- reverse-string/reverseString_2.py
+++ reverse-string/reverseString_2.py
@@ -1,12 +1,8 @@
 def reverseString(str):
-    i = len(str) - 1
-    j = 0
-    while i > j:
-        tmp = str[i]
-        str[i] = str[j]
-        str[j] = tmp
-        i -= 1
-        j += 1
-    return str
+    result = ""
+    for i in range(len(str) - 1, -1, -1):
+        result += str[i]
+    return result
 
-print(reverseString(["h","e","l","l","o"]))
+
+print(reverseString(("hello")))
