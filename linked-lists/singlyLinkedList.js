9e555527640b294ed2e3000bfdadde31c8c060f6
alexandrujeman <alexandrujeman.ja@gmail.com>
2020-01-05 14:42:29 +0200

    Singly linked list constructor and push method

---
+++ linked-lists/singlyLinkedList.js
@@ -0,0 +1,29 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(data) {
+    let newNode = new Node(data);
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+}
+
+let list = new LinkedList();
