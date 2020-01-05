40fae92f2554328b47253e3c1e37316cb2659d43
alexandrujeman <alexandrujeman.ja@gmail.com>
2020-01-05 15:54:42 +0200

    Dubly linked list PUSH .js

---
+++ linked-lists/dublyLinkedList.js
@@ -0,0 +1,30 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(data) {
+    let newNode = new Node(data);
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+}
+
+let list = new LinkedList();
