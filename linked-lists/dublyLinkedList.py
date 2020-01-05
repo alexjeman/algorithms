571cbabda8cd9b76e8032a29efa038681e849ef1
alexandrujeman <alexandrujeman.ja@gmail.com>
2020-01-05 15:54:14 +0200

    Dubly linked list PUSH .py

---
+++ linked-lists/dublyLinkedList.py
@@ -0,0 +1,27 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.prev = None
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+
+    def push(self, data):
+        newNode = Node(data)
+        if self.head == None:
+            self.head = newNode
+            self.tail = newNode
+        else:
+            self.tail.next = newNode
+            newNode.prev = self.tail
+            self.tail = newNode
+        self.length += 1
+        return self
+
+
+list = LinkedList()
