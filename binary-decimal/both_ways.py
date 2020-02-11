d325bc9a6475cc0bad23ae72e73588ea849cd0ff
alexjeman <alexandrujeman.ja@gmail.com>
2020-02-11 09:25:49 +0200

    dec to bin

---
+++ binary-decimal/both_ways.py
@@ -0,0 +1,20 @@
+def decToBin(n):
+    res = []
+    while n:
+        res.append(str(int(n % 2)))
+        n = n // 2
+    return "".join(reversed(res))
+
+
+def binToDec(n):
+    res = 0
+    for i in range(len(n)):
+        res += int(n[len(n) - 1 - i])*pow(2, i)
+    return res
+
+
+# Convert decimal to binary and binary to decimal
+number = 99
+
+print(decToBin(number))
+print(binToDec(decToBin(number)))
