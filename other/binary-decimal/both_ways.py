5c9eed022e1d63bfcd7e9489e0b6ff1a1a9ea34e
alexjeman <alexandrujeman.ja@gmail.com>
2020-02-24 17:45:39 +0200

    algo update

--- other/binary-decimal/both_ways.py
+++ other/binary-decimal/both_ways.py
@@ -1,16 +1,20 @@
-def decToBin(n):
-    res = []
-    while n:
-        res.append(str(int(n % 2)))
-        n = n // 2
-    return "".join(reversed(res))
+# def decToBin(n):
+#     res = []
+#     while n:
+#         res.append(str(int(n % 2)))
+#         n = n // 2
+#     return "".join(reversed(res))
 
+
+def decToBin(n):
+    return format(n, 'b')
 
 # def binToDec(n):
 #     res = 0
 #     for i in range(len(n)):
 #         res += int(n[len(n) - 1 - i])*pow(2, i)
 #     return res
+
 
 def binToDec(n):
     return int(''.join(str(a) for a in n), 2)
